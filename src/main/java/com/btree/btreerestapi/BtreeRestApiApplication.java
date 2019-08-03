4c0792791d8d478792d28f5502b892528a0a3ce4
Jhonatan Zambrano <jzherran@gmail.com>
2019-08-03 14:18:31 -0500

    Fist commit

---
+++ src/main/java/com/btree/btreerestapi/BtreeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.btree.btreerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtreeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtreeRestApiApplication.class, args);
+	}
+
+}
